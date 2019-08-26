51e2bff1b60c03d17353959caf0fc20be4080192
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-26 15:02:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    binding.pry
+    @piglatinier_text = PigLatinizer.new(params["string"])
+#    erb :
+  end
 end
